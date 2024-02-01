5d39791973671a491fb27969dde17b5d450c7457
oguzhan kasimoglu <oguzhankasimoglu@gmail.com>
2024-02-01 16:56:19 +0300

    button

--- src/app/components/ThemeChanger.tsx
+++ src/app/components/ThemeChanger.tsx
@@ -4,15 +4,15 @@ export const ThemeChanger = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex flex-1">
-      <div className="text-black dark:text-white border rounded-3xl">
+    <div className="flex flex-1 gap-2 justify-center items-center m-2">
+      <div className="text-black dark:text-white border border-black dark:border-white rounded-3xl p-2">
         {theme}
         {/* <button onClick={() => setTheme("light")}>Light Mode</button>
       <button onClick={() => setTheme("dark")}>Dark Mode</button> */}
       </div>
-      <div className="">
+      <div className="flex">
         <button
-          className="text-black dark:text-white border rounded-3xl"
+          className="text-black dark:text-white border border-black dark:border-white rounded-3xl p-2"
           onClick={() =>
             theme === "dark" ? setTheme("light") : setTheme("dark")
           }
